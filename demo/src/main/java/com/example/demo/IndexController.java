bd018b75ccd9f32d33d70211a15900f061d4f79d
徐鑫 <xuxin222@qq.com>
2020-05-23 03:49:32 +0800

    aaaa

--- demo/src/main/java/com/example/demo/IndexController.java
+++ demo/src/main/java/com/example/demo/IndexController.java
@@ -10,6 +10,6 @@ public class IndexController {
     @RequestMapping("index")
     @ResponseBody
     public String index(){
-        return "hello idea springboot11";
+        return "hello idea springboot222";
     }
 }
